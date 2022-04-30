304d6f0c80f19f86bfc2c69ac8646c92369b0aab
Saiteja <saitejaerre1913@gmail.com>
2022-04-30 08:53:15 +0530

    first

---
+++ src/test/java/com/wipro/websservive/Phase3EcommerceForSpotyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.websservive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3EcommerceForSpotyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
